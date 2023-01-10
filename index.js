436730e3ea38a5cfba165d1ab97d7164cb2660ba
MaysonD4rk <maysondamarante@hotmail.com>
2023-01-10 14:39:45 -0300

    update

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const app = express();
 const router = require('./routes/routes');
 const cors = require('cors')
 
-app.use(express.urlencoded({ limit: '50mb', extended: true}));
-app.use(cors({ origin: /\.mswareg\.com$/ }))
-app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ limit: '100mb', extended: true}));
+app.use(cors());
+app.use(express.json({limit: '100mb'}));
 app.use('/', router);
 
 app.listen(8000,()=>{
